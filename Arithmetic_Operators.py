36d5b3fbce29a02457a8067f65b5b01cfac9bf25
Sameer-Mohd-Hello <Sameer02032005@gmail.com>
2025-07-17 16:33:52 +0530

    Create Arithmetic_Operators.py

---
+++ Arithmetic_Operators.py
@@ -0,0 +1,22 @@
+a = 10
+b = 5
+# addition opretor
+print(a + b)
+
+# minus opretor
+print(a - b)
+
+# multiply opretor
+print(a * b)
+
+# division opretor
+print(a / b)
+
+# modulo opretor
+print(a % b)
+
+# exponent opretor(power operator)
+print(a**b)
+
+# floor division opretor(returns whole number only)
+print(a // b)
