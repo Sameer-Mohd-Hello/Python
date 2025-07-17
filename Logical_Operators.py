0bdc1ef154954006be4608b8743a98a90459976b
Sameer-Mohd-Hello <Sameer02032005@gmail.com>
2025-07-17 16:44:47 +0530

    Create Logical_Operators.py

---
+++ Logical_Operators.py
@@ -0,0 +1,22 @@
+a=10
+b=20
+# Conpare bits of a and b
+# 1 1 = 1
+# 1 0 = 0
+# 0 1 = 0
+# 0 0 = 0
+print(a&b)
+
+# Conpare bits of a and b
+# 1 1 = 1
+# 1 0 = 1
+# 0 1 = 1
+# 0 0 = 0
+print(a|b)
+
+# Conpare bits of a and b
+# 1 1 = 0
+# 1 0 = 1
+# 0 1 = 1
+# 0 0 = 0
+print(a^b)
