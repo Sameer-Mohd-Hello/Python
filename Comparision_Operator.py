b2d449ccaf10aa231980e7f49c2ebe631c14d6b2
Sameer-Mohd-Hello <Sameer02032005@gmail.com>
2025-07-17 16:41:11 +0530

    Create Comparision_Operator.py

---
+++ Comparision_Operator.py
@@ -0,0 +1,20 @@
+a = 20
+b = 10
+
+# if both equal return true
+print(a == b)
+
+# if both ar not equal return true
+print(a != b)
+
+# if a is less thean b return true
+print(a < b)
+
+# if a is greater than b return true
+print(a > b)
+
+# if a is less or equal thean b return true
+print(a <= b)
+
+# if a is greater or equal than b return true
+print(a >= b)
