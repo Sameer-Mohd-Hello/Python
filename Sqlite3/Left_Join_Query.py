b6a06ae3360b7c2f2ed39592dde2563fea084f30
Sameer-Mohd-Hello <Sameer02032005@gmail.com>
2025-08-09 22:44:58 +0530

    Create Left_Join_Query.py

---
+++ Sqlite3/Left_Join_Query.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+connect = sqlite3.connect("MYDATABASE.db")
+data = connect.execute(
+    "select f.st_Name, f.st_Fees from fees as f left join Student as s on f.st_Id=s.st_Id"
+)
+
+for i in data:
+    print(i)
