fc8c6add1745b32bf03d071ff2ded567cd4bf534
Sameer-Mohd-Hello <Sameer02032005@gmail.com>
2025-09-13 22:18:05 +0530

    Create Avg_Query.py

---
+++ mysql/Avg_Query.py
@@ -0,0 +1,16 @@
+import pymysql as mq
+
+con = mq.connect(host="localhost", user="root", password="", database="echelon")
+cursur = con.cursor()
+
+try:
+    print("{:<10}".format("fees"))
+    que = "select avg(st_fees) from fees "
+
+    cursur.execute(que)
+    data = cursur.fetchall()
+    for a in data:
+        print("{:<10}".format(a[0]))
+
+except Exception as e:
+    print("eror", e)
